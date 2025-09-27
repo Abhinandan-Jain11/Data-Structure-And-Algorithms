6a0b3787f6c409ad9162936aeea1abff8a510ed1
Abhinandan Jain <abhi18aaa@gmail.com>
2025-09-27 12:52:26 +0530

    Time: 14 ms (23.39%), Space: 23 MB (11.89%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -3,7 +3,7 @@ public:
     long long int findHrs(vector<int> &piles, int k){
         long long cnt = 0;
         for(int i=0; i<piles.size(); i++){
-            cnt += ceil((double)piles[i]/(double)k);
+            cnt += ceil((piles[i]+k-1)/k);
         }
         return cnt;
     }
@@ -11,18 +11,16 @@ public:
         int n=piles.size();
         int low=1; 
         int high = *max_element(piles.begin(), piles.end());
-        int ans=INT_MAX;
         while(low<=high){
             int mid =(low+high)/2;
             long long int hrs = findHrs(piles,mid);
             if(hrs <= h){
-                ans = min(ans,mid);
                 high = mid-1;
             }
             else{
                 low = mid+1;
             }
         }
-        return ans;
+        return low;
     }
 };
