ef15ff3350552231c2be913a6f4788c3ca02960d
Abhinandan Jain <abhi18aaa@gmail.com>
2025-09-27 12:56:19 +0530

    Time: 6 ms (76.9%), Space: 23 MB (11.89%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    long long findHrs(const vector<int> &piles, int k){
+    long long findHrs(const vector<int> &piles, long long k){
         long long cnt = 0;
-        for(int i = 0; i < piles.size(); i++){
-            cnt += (piles[i] + k - 1) / k;  
+        for(long long pile : piles){
+            cnt += (pile + k - 1) / k; 
         }
         return cnt;
     }
 
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1;
-        int high = *max_element(piles.begin(), piles.end());
+        long long low = 1;
+        long long high = *max_element(piles.begin(), piles.end());
 
         while(low <= high){
-            int mid = low + (high - low)/2;
+            long long mid = low + (high - low) / 2;
             if(findHrs(piles, mid) <= h){
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return low;
+
+        return (int)low; 
     }
 };
