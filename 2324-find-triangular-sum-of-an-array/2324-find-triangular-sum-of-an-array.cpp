605996bd5887faec44d1659f577b027426967b27
Abhinandan Jain <abhi18aaa@gmail.com>
2025-10-01 12:22:51 +0530

    Time: 33 ms (98.38%), Space: 23.9 MB (47.94%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        while(n>1){
-            vector<int> newNums;
-            for(int j=0; j<n-1; j++){
-                newNums.push_back((nums[j] + nums[j+1])%10);
+        int n = nums.size();
+        vector<int> coeff(n, 0);
+        coeff[0] = 1;
+        for(int i = 1; i < n; i++){
+            for(int j = i; j > 0; j--){
+                coeff[j] = (coeff[j] + coeff[j-1]) % 10;
             }
-            --n;
-            nums = newNums;
         }
-        return nums[0];
+        long long ans = 0;
+        for(int i = 0; i < n; i++){
+            ans = (ans + coeff[i] * nums[i]) % 10;
+        }
+        return ans;
     }
 };
