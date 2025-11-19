46624230c5cf7162b29ba2f101fcadac185e95e2
Abhinandan Jain <abhi18aaa@gmail.com>
2025-11-19 13:57:44 +0530

    Time: 3 ms (43.47%), Space: 15.7 MB (32.4%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
-    int BinarySearch(vector<int>& nums, int original){
-        int n = nums.size();
-        int low = 0; 
-        int high = n-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid]==original){
-                original = 2*original;
-                return BinarySearch(nums,original);
-            }
-            else if(nums[mid]<original){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        while(s.count(original)){
+            original *= 2;
         }
         return original;
-    }
-    int findFinalValue(vector<int>& nums, int original) {
-        sort(nums.begin(),nums.end());
-        return BinarySearch(nums,original);
     }
 };
