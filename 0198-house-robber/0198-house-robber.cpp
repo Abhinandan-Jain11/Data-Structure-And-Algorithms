503aef03eecf80e56ff16cecc02fb13ed7e4c236
Abhinandan Jain <abhi18aaa@gmail.com>
2026-08-25 06:54:26 +0530

    Time: 0 ms (100%), Space: 10.2 MB (79.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int solveTab(int n, vector<int> &nums){
+    int spaceOpt(int n, vector<int> &nums){
         
-        vector<int> dp(n, -1);
-        
-        dp[0] = nums[0];
+        int prev2 = 0;
+        int prev1 = nums[0];
 
         for(int i=1; i<n; i++){
-            int pick = nums[i];
-            if(i>1) pick += dp[i-2];
-            int notPick = dp[i-1];
-            dp[i] = max(pick,notPick);
+            int pick = nums[i] + prev2;
+            int notPick = prev1;
+            int curr = max(pick,notPick);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n-1];
+        return prev1;
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         
-        return solveTab(n,nums);
+        return spaceOpt(n,nums);
     }
 };
