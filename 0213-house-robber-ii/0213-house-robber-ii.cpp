19d92b8f311e33be16259db8ac9856a077d74f9e
Abhinandan Jain <abhi18aaa@gmail.com>
2026-08-25 08:04:23 +0530

    Time: 0 ms (100%), Space: 11.1 MB (7.4%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    int solveMem(int ind,vector<int> &dp,  vector<int> &arr){
-        if(ind==0) return arr[0];
-        if(ind<0) return 0;
+    int solveTab(int n,vector<int> &arr){
+        vector<int> dp(n,-1);
 
-        if(dp[ind]!=-1) return dp[ind];
-
-        int pick = arr[ind] + solveMem(ind-2,dp,arr);
-        int notPick = solveMem(ind-1,dp,arr);
-        dp[ind] = max(pick,notPick);
-
-        return dp[ind];
+        dp[0] = arr[0];
+        for(int i=1; i<n; i++){
+            int pick = arr[i];
+            if(i>1) pick += dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = max(pick,notPick);
+        }
+        return dp[n-1];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==0) return 0;
         if(n==1) return nums[0];
-        vector<int> dp1(n,-1);
-        vector<int> dp2(n,-1);
+        
         vector<int> temp1,temp2;
         for(int i=0; i<n; i++){
             if(i!=0) temp1.push_back(nums[i]);
             if(i!=n-1) temp2.push_back(nums[i]);
         }
-        return max(solveMem(n-2,dp1,temp1),solveMem(n-2,dp2,temp2));
+        return max(solveTab(n-1,temp1),solveTab(n-1,temp2));
     }
 };
