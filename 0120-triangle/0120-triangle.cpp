a2117f79790ea6553063b71f04a654de574b0fe3
Abhinandan Jain <abhi18aaa@gmail.com>
2026-09-16 18:56:31 +0530

    Time: 1 ms (20.3%), Space: 12.8 MB (74.52%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int solveTab(int n, vector<vector<int>> &triangle){
+    int spaceOpt(int n, vector<vector<int>> &triangle){
 
         vector<int> prev(n,0);
+        vector<int> temp(n,0);
 
         for(int j=0; j<n; j++){
             prev[j] = triangle[n-1][j];
         }
         for(int i=n-2; i>=0; i--){
-            vector<int> temp(n,0);
             for(int j=i; j>=0; j--){
                 int down = triangle[i][j] + prev[j];
                 int diagonal = triangle[i][j] + prev[j+1];
@@ -21,6 +21,6 @@ public:
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        return solveTab(n,triangle);
+        return spaceOpt(n,triangle);
     }
 };
