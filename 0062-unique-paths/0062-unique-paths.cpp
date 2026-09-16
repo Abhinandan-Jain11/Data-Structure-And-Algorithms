6558e6ecde8fb427adee2410a62eb0bf0bceaa25
Abhinandan Jain <abhi18aaa@gmail.com>
2026-09-16 08:23:11 +0530

    Time: 0 ms (100%), Space: 9.5 MB (30.31%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    int solveMem(int r, int c, vector<vector<int>> & dp){
+    int solveTab(int m, int n){
 
-        if(r==0 && c==0) return 1;
-        if(r<0 || c<0) return 0;
+        vector<vector<int>> dp(m , vector<int>(n, 0));
 
-        if(dp[r][c] != -1) return dp[r][c];
+        dp[0][0] = 1;
 
-        int up = solveMem(r-1,c,dp);
-        int left = solveMem(r,c-1,dp);
-
-        return dp[r][c] = up + left;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0) continue;
+                int down=0;
+                int right=0;
+                if(i>0){
+                    down = dp[i-1][j];
+                }
+                if(j>0){
+                    right = dp[i][j-1];
+                }  
+                dp[i][j] = down + right;
+            }
+        }
+        return dp[m-1][n-1];
     }
     int uniquePaths(int m, int n) {
-       vector<vector<int>> dp(m , vector<int>(n, -1));
-       return solveMem(m-1,n-1,dp);
+       
+       return solveTab(m,n);
     }
 };
