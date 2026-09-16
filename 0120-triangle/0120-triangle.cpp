7f93492ff5a8e83ba2688bf313f8a349cb7962a4
Abhinandan Jain <abhi18aaa@gmail.com>
2026-09-16 18:54:09 +0530

    Time: 3 ms (13.26%), Space: 13 MB (63.9%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int solveTab(int n, vector<vector<int>> &triangle){
 
-        vector<vector<int>> dp(n, vector<int> (n,-1));
+        vector<int> prev(n,0);
 
         for(int j=0; j<n; j++){
-            dp[n-1][j] = triangle[n-1][j];
+            prev[j] = triangle[n-1][j];
         }
         for(int i=n-2; i>=0; i--){
+            vector<int> temp(n,0);
             for(int j=i; j>=0; j--){
-                int down = triangle[i][j] + dp[i+1][j];
-                int diagonal = triangle[i][j] + dp[i+1][j+1];
+                int down = triangle[i][j] + prev[j];
+                int diagonal = triangle[i][j] + prev[j+1];
 
-                dp[i][j] = min(down,diagonal);
+                temp[j] = min(down,diagonal);
             }
+            prev = temp;
         }
-        return dp[0][0];
+        return prev[0];
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
