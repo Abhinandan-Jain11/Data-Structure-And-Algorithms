2015f4aa592244535b1fd496041a04249584bab9
Abhinandan Jain <abhi18aaa@gmail.com>
2026-09-16 11:40:43 +0530

    Time: 0 ms (100%), Space: 15.9 MB (74.85%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int solveTab(int m, int n, vector<vector<int>> &grid){
+    int spaceOpt(int m, int n, vector<vector<int>> &grid){
 
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-
-        dp[0][0] = grid[0][0];
+        vector<int> prev(n,0);
 
         for(int i=0; i<m; i++){
+            vector<int> temp(n,0);
             for(int j=0; j<n; j++){
-                int up = 0;
-                int left = 0;
                 if(i==0 && j==0){
+                    temp[0] = grid[0][0];
                     continue;
                 }
-                if(i>0) up = grid[i][j] + dp[i-1][j];
+                int up = 0;
+                int left = 0;
+                if(i>0) up = grid[i][j] + prev[j];
                 else up = INT_MAX;
-                if(j>0) left = grid[i][j] + dp[i][j-1];
+                if(j>0) left = grid[i][j] + temp[j-1];
                 else left = INT_MAX;
-                dp[i][j] = min(up,left);
+                temp[j] = min(up,left);
             }
+            prev = temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        return solveTab(m,n,grid);
+        return spaceOpt(m,n,grid);
     }
 };
