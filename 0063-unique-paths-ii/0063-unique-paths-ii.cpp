80e0c3079d6221edd37f6d159492916cb19b364b
Abhinandan Jain <abhi18aaa@gmail.com>
2026-09-16 10:04:46 +0530

    Time: 0 ms (100%), Space: 11.6 MB (77.8%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    int solveMem(int m, int n, vector<vector<int>> &obstacleGrid){
+    int solveTab(int m, int n, vector<vector<int>> &obstacleGrid){
 
-        vector<vector<int>> dp(m , vector<int>(n, -1));
-
-        dp[0][0] = 1;
+        vector<int> prev(n,0);
 
         for(int i=0; i<m; i++){
+            vector<int> temp(n,0);
             for(int j=0; j<n; j++){
                 int up = 0;
                 int left = 0;
                 if(obstacleGrid[i][j] == 1){
-                    dp[i][j] = 0;
+                    temp[j] = 0;
                     continue;
                 }
                 if(i==0 && j==0){
+                    temp[0] = 1;
                     continue;
                 }else{
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
+                    up = prev[j];
+                    if(j>0) left = temp[j-1];
                 }
-                dp[i][j] = up + left;
+                temp[j] = up + left;
             }
+            prev = temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        return solveMem(m,n,obstacleGrid);
+        return solveTab(m,n,obstacleGrid);
     }
 };
