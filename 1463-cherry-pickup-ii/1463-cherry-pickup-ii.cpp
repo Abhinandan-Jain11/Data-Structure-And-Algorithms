a703d0103dac16ca7671e9aef25ea856707146e3
Abhinandan Jain <abhi18aaa@gmail.com>
2026-09-21 21:19:59 +0530

    Time: 66 ms (25.39%), Space: 21.1 MB (14.26%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
-    int solveMem(int i, int j1, int j2, int n, int m, vector<vector<vector<int>>>&dp, vector<vector<int>> &grid){
-        if(i>n-1 || j1<0 || j1>m-1 || j2<0 || j2>m-1) return -1e8;
-
-        if(i == n-1){
-            if(j1==j2) return grid[i][j1];
-            else return grid[i][j1] + grid[i][j2];
-        }
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
-        int maxi = -1e8;
-        for(int dj1=-1; dj1<=1; dj1++){
-            for(int dj2=-1; dj2<=1; dj2++){
-                int cherries;
-
-                if (j1 == j2){
-                    cherries = grid[i][j1];
-                }else{
-                    cherries = grid[i][j1] + grid[i][j2];
-                }    
-                cherries += solveMem(i + 1,j1 + dj1,j2 + dj2,n, m, dp, grid);
-                maxi = max(maxi, cherries);
-            }
-        }
-        return dp[i][j1][j2] = maxi;
-    }
-    int cherryPickup(vector<vector<int>>& grid) {
+    int solveTab(vector<vector<int>> &grid){
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<vector<int>>> dp(n,vector<vector<int>> (m,vector<int> (m,-1)));
-        return solveMem(0,0,m-1,n,m,dp,grid);
+        for(int j1=0; j1<m; j1++){
+            for(int j2=0; j2<m; j2++){
+                if(j1==j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j1=0; j1<m; j1++){
+                for(int j2=0; j2<m; j2++){
+                    int maxi = -1e8;
+                    for(int dj1=-1; dj1<=1; dj1++){
+                        for(int dj2=-1; dj2<=1; dj2++){
+                            int cherries = 0;
+                            if (j1 == j2){
+                                cherries = grid[i][j1];
+                            }else{
+                                cherries = grid[i][j1] + grid[i][j2];
+                            }
+                            if(j1+dj1>=0 && j1+dj1<m && j2+dj2>=0 && j2+dj2<m){
+                                cherries += dp[i+1][j1+dj1][j2+dj2];
+                            }else{
+                                cherries += -1e8;
+                            }
+                            maxi = max(maxi, cherries);    
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        return solveTab(grid);
     }
 };
